8f7326f9b2e8d7879b40a815b7344598de3e2ac9
寒兰 <@DESKTOP-BV8IOV4>
2018-05-20 11:25:26 +0800

    张涛

--- HOZAPWorkStation/Form1.Designer.cs
+++ HOZAPWorkStation/Form1.Designer.cs
@@ -31,6 +31,7 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InitialInterface));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -51,11 +52,21 @@
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 129);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(89, 85);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // InitialInterface
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(667, 345);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -73,6 +84,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- HOZAPWorkStation/Form1.cs
+++ HOZAPWorkStation/Form1.cs
@@ -16,5 +16,7 @@ namespace HOZAPWorkStation
         {
             InitializeComponent();
         }
+
+       
     }
 }
