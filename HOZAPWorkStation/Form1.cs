74c9ffc44bb2b6b4450dfb2a6570c7227735030f
YangG <18341312561@163.com>
2018-05-20 11:02:18 +0800

    Yang.G

---
+++ HOZAPWorkStation/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HOZAPWorkStation
+{
+    public partial class InitialInterface : Form
+    {
+        public InitialInterface()
+        {
+            InitializeComponent();
+        }
+    }
+}
