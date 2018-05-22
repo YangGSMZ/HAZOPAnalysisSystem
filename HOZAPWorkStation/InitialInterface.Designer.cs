dc4fce2ce0bcdfc7fc9e0e52255387625a824dc4
githubHanLan <869799249@qqcom>
2018-05-22 17:33:34 +0800

    张涛

--- HOZAPWorkStation/InitialInterface.Designer.cs
+++ HOZAPWorkStation/InitialInterface.Designer.cs
@@ -35,10 +35,14 @@
             this.openBtn = new System.Windows.Forms.Button();
             this.preBtn = new System.Windows.Forms.Button();
             this.nodeBtn = new System.Windows.Forms.Button();
-            this.analysisBtn = new System.Windows.Forms.Button();
             this.printBtn = new System.Windows.Forms.Button();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.analysisBtn = new System.Windows.Forms.Button();
             this.newBtn = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
             this.initMenuStrip.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
             this.SuspendLayout();
             // 
             // initMenuStrip
@@ -68,7 +72,7 @@
             // openBtn
             // 
             this.openBtn.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.openBtn.Location = new System.Drawing.Point(640, 268);
+            this.openBtn.Location = new System.Drawing.Point(94, 48);
             this.openBtn.Name = "openBtn";
             this.openBtn.Size = new System.Drawing.Size(75, 75);
             this.openBtn.TabIndex = 4;
@@ -78,7 +82,7 @@
             // preBtn
             // 
             this.preBtn.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.preBtn.Location = new System.Drawing.Point(640, 363);
+            this.preBtn.Location = new System.Drawing.Point(213, 130);
             this.preBtn.Name = "preBtn";
             this.preBtn.Size = new System.Drawing.Size(75, 75);
             this.preBtn.TabIndex = 5;
@@ -88,54 +92,78 @@
             // nodeBtn
             // 
             this.nodeBtn.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.nodeBtn.Location = new System.Drawing.Point(500, 268);
+            this.nodeBtn.Location = new System.Drawing.Point(333, 130);
             this.nodeBtn.Name = "nodeBtn";
             this.nodeBtn.Size = new System.Drawing.Size(75, 75);
             this.nodeBtn.TabIndex = 6;
             this.nodeBtn.Text = "节点";
             this.nodeBtn.UseVisualStyleBackColor = false;
             // 
-            // analysisBtn
-            // 
-            this.analysisBtn.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.analysisBtn.Location = new System.Drawing.Point(500, 163);
-            this.analysisBtn.Name = "analysisBtn";
-            this.analysisBtn.Size = new System.Drawing.Size(75, 75);
-            this.analysisBtn.TabIndex = 7;
-            this.analysisBtn.Text = "分析";
-            this.analysisBtn.UseVisualStyleBackColor = false;
-            // 
             // printBtn
             // 
             this.printBtn.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.printBtn.Location = new System.Drawing.Point(640, 163);
+            this.printBtn.Location = new System.Drawing.Point(591, 130);
             this.printBtn.Name = "printBtn";
             this.printBtn.Size = new System.Drawing.Size(75, 75);
             this.printBtn.TabIndex = 8;
             this.printBtn.Text = "打印";
             this.printBtn.UseVisualStyleBackColor = false;
             // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.LightGray;
+            this.tabPage1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.tabPage1.Controls.Add(this.newBtn);
+            this.tabPage1.Controls.Add(this.preBtn);
+            this.tabPage1.Controls.Add(this.printBtn);
+            this.tabPage1.Controls.Add(this.analysisBtn);
+            this.tabPage1.Controls.Add(this.nodeBtn);
+            this.tabPage1.Controls.Add(this.openBtn);
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(792, 361);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "系统首页";
+            // 
+            // analysisBtn
+            // 
+            this.analysisBtn.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.analysisBtn.Location = new System.Drawing.Point(453, 130);
+            this.analysisBtn.Name = "analysisBtn";
+            this.analysisBtn.Size = new System.Drawing.Size(75, 75);
+            this.analysisBtn.TabIndex = 7;
+            this.analysisBtn.Text = "分析";
+            this.analysisBtn.UseVisualStyleBackColor = false;
+            // 
             // newBtn
             // 
             this.newBtn.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.newBtn.Location = new System.Drawing.Point(500, 82);
+            this.newBtn.Location = new System.Drawing.Point(94, 211);
             this.newBtn.Name = "newBtn";
             this.newBtn.Size = new System.Drawing.Size(75, 75);
             this.newBtn.TabIndex = 11;
             this.newBtn.Text = "新建";
             this.newBtn.UseVisualStyleBackColor = false;
             // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Font = new System.Drawing.Font("宋体", 10F);
+            this.tabControl1.ItemSize = new System.Drawing.Size(70, 25);
+            this.tabControl1.Location = new System.Drawing.Point(0, 56);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(800, 394);
+            this.tabControl1.TabIndex = 13;
+            // 
             // InitialInterface
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.newBtn);
-            this.Controls.Add(this.printBtn);
-            this.Controls.Add(this.analysisBtn);
-            this.Controls.Add(this.nodeBtn);
-            this.Controls.Add(this.preBtn);
-            this.Controls.Add(this.openBtn);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.initPanel);
             this.Controls.Add(this.initMenuStrip);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -147,6 +175,8 @@
             this.Resize += new System.EventHandler(this.InitialInterface_Resize);
             this.initMenuStrip.ResumeLayout(false);
             this.initMenuStrip.PerformLayout();
+            this.tabPage1.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -160,8 +190,10 @@
         private System.Windows.Forms.Button openBtn;
         private System.Windows.Forms.Button preBtn;
         private System.Windows.Forms.Button nodeBtn;
-        private System.Windows.Forms.Button analysisBtn;
         private System.Windows.Forms.Button printBtn;
+        private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.Button newBtn;
+        private System.Windows.Forms.Button analysisBtn;
+        private System.Windows.Forms.TabControl tabControl1;
     }
 }
